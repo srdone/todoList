d96cc226a7f87049ceaca29b95b626b57f425da3
DANIEL BULLOCK <DANIEL.I.BULLOCK@gmail.com>
2015-01-28 19:12:42 -0700

    "finished to do list"

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 $(document).ready(function () {
-
-    $('button').on('click', function () {
+    // select submit button for actions
+    $('#submit').on('click', function () {
         // store list item to variable *item*
         var item = $('#newItem').val();
         // clear input for new item
         $('#newItem').val(''); 
         // add new item if string length is more then 0
         if (item.length > 0) {
-            
-            $('#list').append('<li>' + item + '</li>');
+            // import distinction between append and html methods, try both to see what happens
+            $('#list').append('<li>' + item + '</>');
         }
         
     });
 
 
+    $('#list').on('dblclick','li', function () {
+        $(this).remove();
+    });
 
+    $('#list').on('click', 'li', function () {
+        $(this).toggleClass('done');
+    });
 
 
 
